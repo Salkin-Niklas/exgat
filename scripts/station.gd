52982588c5b07a36fa2d14d49372419a89d5fcd0
unknown <nizdvfc@web.de>
2025-10-04 12:25:18 +0200

    ADD: destructible station

---
+++ scripts/station.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var health: float = 0
+
+signal health_changed(val: float)
+signal gameover()
+
+func _process(_delta: float) -> void:
+	if health<=0:
+		emit_signal("gameover")
+	$Sprite2D.texture = preload("res://assets/station/station.png")
+
+func _on_area_shape_entered(_area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	var other_shape_owner = area.shape_find_owner(area_shape_index)
+	var other_shape_node = area.shape_owner_get_owner(other_shape_owner)
+	other_shape_node.get_parent().destroy()
+
+	var local_shape_owner = shape_find_owner(local_shape_index)
+	var local_shape_node = shape_owner_get_owner(local_shape_owner)
+	local_shape_node.get_child(0).texture = preload("res://assets/station/panel.png")
+	health -= 1
+	emit_signal("health_changed", health)
+	print("station damaged, ", health)
---
+++ scripts/world.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $Game/Player
+@onready var station: Area2D = $Game/Station
