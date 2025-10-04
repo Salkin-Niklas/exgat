628883937fc3a540c8199f61493bba25eaae6072
unknown <nizdvfc@web.de>
2025-10-04 12:52:32 +0200

    UPDATE: gui and station

--- scripts/station.gd
+++ scripts/station.gd
@@ -9,6 +9,7 @@ func _process(_delta: float) -> void:
 	if health<=0:
 		emit_signal("gameover")
 	$Sprite2D.texture = preload("res://assets/station/station.png")
+	$Sprite2D.hide()
 
 func _on_area_shape_entered(_area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	var other_shape_owner = area.shape_find_owner(area_shape_index)
@@ -18,6 +19,8 @@ func _on_area_shape_entered(_area_rid: RID, area: Area2D, area_shape_index: int,
 	var local_shape_owner = shape_find_owner(local_shape_index)
 	var local_shape_node = shape_owner_get_owner(local_shape_owner)
 	local_shape_node.get_child(0).texture = preload("res://assets/station/panel.png")
+	local_shape_node.get_child(0).texture.hide()
+	local_shape_node.disabled = true
 	health -= 1
 	emit_signal("health_changed", health)
 	print("station damaged, ", health)
